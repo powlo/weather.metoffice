4caa1a2f712d4bfcbf5ea40518a846fc8800c8be
Paul Backhouse <paul_backhouse@hotmail.com>
2014-02-13 19:27:07 +0000

    Remove unused functions

--- resources/lib/datapoint.py
+++ resources/lib/datapoint.py
@@ -2,7 +2,7 @@
 #http://www.metoffice.gov.uk/datapoint
 import urllib
 import urllib2
-from utilities import retryurlopen, log
+from utilities import log
 
 URL_TEMPLATE = "http://datapoint.metoffice.gov.uk/public/data/{format}/{resource}/{group}/{datatype}/{object}?{get}"
 
@@ -41,64 +41,3 @@ LONG_REGIONAL_NAMES = {'os': 'Orkney and Shetland',
                        'sw': 'Southwest England',
                        'wl': 'Wales',
                        'uk': 'United Kingdom'}
-
-def get_sitelist(location):
-    log("Getting sitelist for '%s'" % location)
-    url_params = {
-        'ForecastLocation' : {'resource' : 'wxfcs'},
-        'ObservationLocation' : {'resource' : 'wxobs'},
-        'RegionalLocation' : {'resource' : 'wxfcs', 'format': 'txt', 'group' : 'regionalforecast'},
-        }
-    args = url_params[location]
-    args.update({'params':{'key': API_KEY}})
-    url = url(**args)
-    log("URL: %s" % url)
-    page = utilities.retryurlopen(url).decode('latin-1')
-    data = json.loads(page)
-    sitelist = data['Locations']['Location']
-    if location == 'RegionalLocation':
-        #bug in datapoint: sitelist requires cleaning for regional forecast
-        sitelist = clean_sitelist(sitelist)
-        #long names are more user friendly
-        for site in sitelist:
-            site['name'] = LONG_REGIONAL_NAMES[site['name']]
-    return sitelist
-
-def clean_sitelist(sitelist):
-    """
-    A bug in datapoint returns keys prefixed with '@'
-    This func chops them out
-    """
-    new_sites = []
-    new_site = {}
-
-    for site in sitelist:
-        for key in site:
-           if key.startswith('@'):
-               new_key = key[1:]
-               new_site[new_key] = site[key]
-        new_sites.append(new_site.copy())
-    return new_sites
-
-def filter_sitelist(text, sitelist):
-    """
-    Takes a list of dictionaries and returns only
-    those entries whose 'name' key contains text
-    """
-    filteredsitelist = list()
-    for x in sitelist:
-        if x['name'].lower().find(text.lower()) != -1:
-            filteredsitelist.append(x)
-    return filteredsitelist
-
-#get data from datapoint
-def url(format='val', resource='wxobs', group='all', datatype='json', object='sitelist', params={}):
-    #todo: validate parameters
-    get_params = urllib.urlencode(params)
-    substitute = {'format': format,
-                  'resource': resource,
-                  'group': group,
-                  'datatype': datatype,
-                  'object': object,
-                  'params': get_params}
-    return BASE_URL + RESOURCE_URL % substitute
--- resources/lib/utilities.py
+++ resources/lib/utilities.py
@@ -21,26 +21,6 @@ CACHE_FILE = os.path.join(ADDON_DATA_PATH, 'cache.json')
 def log(msg, level=xbmc.LOGNOTICE):
     xbmc.log("%s: %s" %(__addonid__, msg), level)
 
-def retryurlopen(url, retry=3):
-    """
-    A helper function to retry a url a number of times
-    """
-    while True:
-        try:
-            return urllib2.urlopen(url, timeout=1).read()
-        except:
-            if retry:
-                retry -= 1
-            else:
-                raise
-
-def dewpoint_temp(temp, humidity):
-    """
-    Approximate dewpoint using simple approximation
-    http://en.wikipedia.org/wiki/Dew_point#Simple_approximation
-    """
-    return str(int(temp) - ((100 - int(humidity))/5))
-
 def day_name(date):
     """
     Takes a date and returns the day of the week as a string
@@ -53,19 +33,3 @@ def minutes_as_time(minutes):
     as a time, starting at midnight.
     """
     return time.strftime('%H:%M', time.gmtime(minutes*60))
-
-def verbose_timedelta(delta):
-    hours, remainder = divmod(delta.seconds, 3600)
-    minutes, seconds = divmod(remainder, 60)
-    dstr = "%s day%s" % (delta.days, "s" if delta.days != 1 else "")
-    hstr = "%s hour%s" % (hours, "s" if hours!=1 else "")
-    mstr = "%s minute%s" % (minutes, "s" if minutes != 1 else "")
-    sstr = "%s second%s" % (seconds, "s" if seconds != 1 else "")
-    dhms = [dstr, hstr, mstr, sstr]
-    for _ in range(2):
-        for x, y in enumerate(dhms):
-            if not y.startswith('0'):
-                dhms = dhms[x:]
-                break
-        dhms.reverse()
-    return ', '.join(dhms)
