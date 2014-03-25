480330282a2da955db8eda850ca8234678c45985
Paul Backhouse <paul_backhouse@hotmail.com>
2014-03-25 15:45:58 +0000

    Modify KeyError exception to provide something mildly meaninful to display

--- src/metoffice/properties.py
+++ src/metoffice/properties.py
@@ -19,27 +19,31 @@ def observation():
     with urlcache.URLCache(ADDON_DATA_PATH) as cache:
         filename = cache.get(HOURLY_LOCATION_OBSERVATION_URL, observation_expiry)
         data=json.load(open(filename))
-    dv = data['SiteRep']['DV']
-    dataDate = dv.get('dataDate').rstrip('Z')
-    WINDOW.setProperty('HourlyObservation.IssuedAt', time.strftime(ISSUEDAT_FORMAT, time.strptime(dataDate, DATAPOINT_DATETIME_FORMAT)))#@UndefinedVariable
     try:
-        latest_period = dv['Location']['Period'][-1]
-    except KeyError:
-        latest_period = dv['Location']['Period']
-    try:
-        latest_obs = latest_period['Rep'][-1]
-    except KeyError:
-        latest_obs = latest_period['Rep']
-    WINDOW.setProperty('Current.Condition', WEATHER_CODES[latest_obs.get('W', 'na')][1])#@UndefinedVariable
-    WINDOW.setProperty('Current.Visibility', latest_obs.get('V', 'n/a'))#@UndefinedVariable
-    WINDOW.setProperty('Current.Pressure', latest_obs.get('P', 'n/a'))#@UndefinedVariable
-    WINDOW.setProperty('Current.Temperature', str(round(float(latest_obs.get('T', 'n/a')))).split('.')[0])#@UndefinedVariable
-    WINDOW.setProperty('Current.Wind', latest_obs.get('S', 'n/a'))#@UndefinedVariable
-    WINDOW.setProperty('Current.WindDirection', latest_obs.get('D', 'n/a'))#@UndefinedVariable
-    WINDOW.setProperty('Current.WindGust', latest_obs.get('G', 'n/a'))#@UndefinedVariable
-    WINDOW.setProperty('Current.OutlookIcon', '%s.png' % WEATHER_CODES[latest_obs.get('W', 'na')][0])#@UndefinedVariable
-    WINDOW.setProperty('Current.FanartCode', '%s.png' % WEATHER_CODES[latest_obs.get('W','na')][0])#@UndefinedVariable
-    WINDOW.setProperty('HourlyObservation.IsFetched', 'true')#@UndefinedVariable
+        dv = data['SiteRep']['DV']
+        dataDate = dv.get('dataDate').rstrip('Z')
+        WINDOW.setProperty('HourlyObservation.IssuedAt', time.strftime(ISSUEDAT_FORMAT, time.strptime(dataDate, DATAPOINT_DATETIME_FORMAT)))#@UndefinedVariable
+        try:
+            latest_period = dv['Location']['Period'][-1]
+        except KeyError:
+            latest_period = dv['Location']['Period']
+        try:
+            latest_obs = latest_period['Rep'][-1]
+        except KeyError:
+            latest_obs = latest_period['Rep']
+        WINDOW.setProperty('Current.Condition', WEATHER_CODES[latest_obs.get('W', 'na')][1])#@UndefinedVariable
+        WINDOW.setProperty('Current.Visibility', latest_obs.get('V', 'n/a'))#@UndefinedVariable
+        WINDOW.setProperty('Current.Pressure', latest_obs.get('P', 'n/a'))#@UndefinedVariable
+        WINDOW.setProperty('Current.Temperature', str(round(float(latest_obs.get('T', 'n/a')))).split('.')[0])#@UndefinedVariable
+        WINDOW.setProperty('Current.Wind', latest_obs.get('S', 'n/a'))#@UndefinedVariable
+        WINDOW.setProperty('Current.WindDirection', latest_obs.get('D', 'n/a'))#@UndefinedVariable
+        WINDOW.setProperty('Current.WindGust', latest_obs.get('G', 'n/a'))#@UndefinedVariable
+        WINDOW.setProperty('Current.OutlookIcon', '%s.png' % WEATHER_CODES[latest_obs.get('W', 'na')][0])#@UndefinedVariable
+        WINDOW.setProperty('Current.FanartCode', '%s.png' % WEATHER_CODES[latest_obs.get('W','na')][0])#@UndefinedVariable
+        WINDOW.setProperty('HourlyObservation.IsFetched', 'true')#@UndefinedVariable
+    except KeyError as e:
+        e.args = ("Key Error in JSON File", "Key '{0}' not found while processing file from url:".format(e.args[0]), HOURLY_LOCATION_OBSERVATION_URL)
+        raise
 
 @utilities.panelbusy('RightPane')
 def daily():
@@ -47,21 +51,26 @@ def daily():
     with urlcache.URLCache(ADDON_DATA_PATH) as cache:
         filename = cache.get(DAILY_LOCATION_FORECAST_URL, daily_expiry)
         data=json.load(open(filename))
-    dv = data['SiteRep']['DV']
-    dataDate = dv.get('dataDate').rstrip('Z')
-    WINDOW.setProperty('DailyForecast.IssuedAt', time.strftime(ISSUEDAT_FORMAT, time.strptime(dataDate, DATAPOINT_DATETIME_FORMAT))) #@UndefinedVariable
-    for p, period in enumerate(dv['Location']['Period']):
-        WINDOW.setProperty('Day%d.Title' %p, time.strftime(SHORT_DAY_FORMAT, time.strptime(period.get('value'), DATAPOINT_DATE_FORMAT)))#@UndefinedVariable
-        for rep in period['Rep']:
-            weather_type = rep.get('W', 'na')
-            if rep.get('$') == 'Day':
-                WINDOW.setProperty('Day%d.HighTemp' %p, rep.get('Dm', 'na'))#@UndefinedVariable
-                WINDOW.setProperty('Day%d.Outlook' %p, WEATHER_CODES.get(weather_type)[1])#@UndefinedVariable
-                WINDOW.setProperty('Day%d.OutlookIcon' % p, WEATHER_ICON_PATH % WEATHER_CODES.get(weather_type, 'na')[0])#@UndefinedVariable
-                WINDOW.setProperty('Day%d.WindSpeed' % p,  rep.get('S', 'na'))#@UndefinedVariable
-                WINDOW.setProperty('Day%d.WindDirection' % p, rep.get('D', 'na').lower())#@UndefinedVariable
-            elif rep.get('$') == 'Night':
-                WINDOW.setProperty('Day%d.LowTemp' %p, rep.get('Nm', 'na'))#@UndefinedVariable
+    try:
+        dv = data['SiteRep']['DV']
+        dataDate = dv.get('dataDate').rstrip('Z')
+        WINDOW.setProperty('DailyForecast.IssuedAt', time.strftime(ISSUEDAT_FORMAT, time.strptime(dataDate, DATAPOINT_DATETIME_FORMAT))) #@UndefinedVariable
+        for p, period in enumerate(dv['Location']['Period']):
+            WINDOW.setProperty('Day%d.Title' %p, time.strftime(SHORT_DAY_FORMAT, time.strptime(period.get('value'), DATAPOINT_DATE_FORMAT)))#@UndefinedVariable
+            for rep in period['Rep']:
+                weather_type = rep.get('W', 'na')
+                if rep.get('$') == 'Day':
+                    WINDOW.setProperty('Day%d.HighTemp' %p, rep.get('Dm', 'na'))#@UndefinedVariable
+                    WINDOW.setProperty('Day%d.Outlook' %p, WEATHER_CODES.get(weather_type)[1])#@UndefinedVariable
+                    WINDOW.setProperty('Day%d.OutlookIcon' % p, WEATHER_ICON_PATH % WEATHER_CODES.get(weather_type, 'na')[0])#@UndefinedVariable
+                    WINDOW.setProperty('Day%d.WindSpeed' % p,  rep.get('S', 'na'))#@UndefinedVariable
+                    WINDOW.setProperty('Day%d.WindDirection' % p, rep.get('D', 'na').lower())#@UndefinedVariable
+                elif rep.get('$') == 'Night':
+                    WINDOW.setProperty('Day%d.LowTemp' %p, rep.get('Nm', 'na'))#@UndefinedVariable
+    except KeyError as e:
+        e.args = ("Key Error in JSON File", "Key '{0}' not found while processing file from url:".format(e.args[0]), DAILY_LOCATION_FORECAST_URL)
+        raise
+
     WINDOW.setProperty('DailyForecast.IsFetched', 'true')#@UndefinedVariable
 
 @utilities.panelbusy('RightPane')
@@ -70,26 +79,30 @@ def threehourly():
     with urlcache.URLCache(ADDON_DATA_PATH) as cache:
         filename = cache.get(THREEHOURLY_LOCATION_FORECAST_URL, threehourly_expiry)
         data=json.load(open(filename))
-    dv = data['SiteRep']['DV']
-    dataDate = dv.get('dataDate').rstrip('Z')
-    WINDOW.setProperty('3HourlyForecast.IssuedAt', time.strftime(ISSUEDAT_FORMAT, time.strptime(dataDate, DATAPOINT_DATETIME_FORMAT)))#@UndefinedVariable
-    count = 0
-    for period in dv['Location']['Period']:
-        for rep in period['Rep']:
-            #extra xbmc targeted info:
-            weather_type = rep.get('W', 'na')
-            WINDOW.setProperty('3Hourly%d.Outlook' % count, WEATHER_CODES.get(weather_type)[1])#@UndefinedVariable
-            WINDOW.setProperty('3Hourly%d.WindSpeed' % count, rep.get('S', 'n/a'))#@UndefinedVariable
-            WINDOW.setProperty('3Hourly%d.WindDirection' % count, rep.get('D', 'na').lower())#@UndefinedVariable
-            WINDOW.setProperty('3Hourly%d.GustSpeed' % count, rep.get('G', 'n/a'))#@UndefinedVariable
-            WINDOW.setProperty('3Hourly%d.UVIndex' % count, rep.get('U', 'n/a'))#@UndefinedVariable
-            WINDOW.setProperty('3Hourly%d.Precipitation' % count, rep.get('Pp'))#@UndefinedVariable
-            WINDOW.setProperty('3Hourly%d.OutlookIcon' % count, WEATHER_ICON_PATH % WEATHER_CODES.get(weather_type, 'na')[0])#@UndefinedVariable
-            WINDOW.setProperty('3Hourly%d.Day' % count, time.strftime(SHORT_DAY_FORMAT, time.strptime(period.get('value'), DATAPOINT_DATE_FORMAT)))#@UndefinedVariable
-            WINDOW.setProperty('3Hourly%d.Time' % count, utilities.minutes_as_time(int(rep.get('$'))))#@UndefinedVariable
-            WINDOW.setProperty('3Hourly%d.ActualTemp' % count, rep.get('T', 'na'))#@UndefinedVariable
-            WINDOW.setProperty('3Hourly%d.FeelsLikeTemp' % count, rep.get('F', 'na'))#@UndefinedVariable
-            count +=1
+    try:
+        dv = data['SiteRep']['DV']
+        dataDate = dv.get('dataDate').rstrip('Z')
+        WINDOW.setProperty('3HourlyForecast.IssuedAt', time.strftime(ISSUEDAT_FORMAT, time.strptime(dataDate, DATAPOINT_DATETIME_FORMAT)))#@UndefinedVariable
+        count = 0
+        for period in dv['Location']['Period']:
+            for rep in period['Rep']:
+                #extra xbmc targeted info:
+                weather_type = rep.get('W', 'na')
+                WINDOW.setProperty('3Hourly%d.Outlook' % count, WEATHER_CODES.get(weather_type)[1])#@UndefinedVariable
+                WINDOW.setProperty('3Hourly%d.WindSpeed' % count, rep.get('S', 'n/a'))#@UndefinedVariable
+                WINDOW.setProperty('3Hourly%d.WindDirection' % count, rep.get('D', 'na').lower())#@UndefinedVariable
+                WINDOW.setProperty('3Hourly%d.GustSpeed' % count, rep.get('G', 'n/a'))#@UndefinedVariable
+                WINDOW.setProperty('3Hourly%d.UVIndex' % count, rep.get('U', 'n/a'))#@UndefinedVariable
+                WINDOW.setProperty('3Hourly%d.Precipitation' % count, rep.get('Pp'))#@UndefinedVariable
+                WINDOW.setProperty('3Hourly%d.OutlookIcon' % count, WEATHER_ICON_PATH % WEATHER_CODES.get(weather_type, 'na')[0])#@UndefinedVariable
+                WINDOW.setProperty('3Hourly%d.Day' % count, time.strftime(SHORT_DAY_FORMAT, time.strptime(period.get('value'), DATAPOINT_DATE_FORMAT)))#@UndefinedVariable
+                WINDOW.setProperty('3Hourly%d.Time' % count, utilities.minutes_as_time(int(rep.get('$'))))#@UndefinedVariable
+                WINDOW.setProperty('3Hourly%d.ActualTemp' % count, rep.get('T', 'na'))#@UndefinedVariable
+                WINDOW.setProperty('3Hourly%d.FeelsLikeTemp' % count, rep.get('F', 'na'))#@UndefinedVariable
+                count +=1
+    except KeyError as e:
+        e.args = ("Key Error in JSON File", "Key '{0}' not found while processing file from url:".format(e.args[0]), THREEHOURLY_LOCATION_FORECAST_URL)
+        raise
     WINDOW.setProperty('3HourlyForecast.IsFetched', 'true')#@UndefinedVariable
 
 @utilities.panelbusy('RightPane')
@@ -98,21 +111,25 @@ def text():
     with urlcache.URLCache(ADDON_DATA_PATH) as cache:
         filename = cache.get(TEXT_FORECAST_URL, text_expiry)
         data=json.load(open(filename))
-    rf = data['RegionalFcst']
-    issuedat = rf['issuedAt'].rstrip('Z')
-    WINDOW.setProperty('TextForecast.IssuedAt', time.strftime(ISSUEDAT_FORMAT, time.strptime(issuedat, DATAPOINT_DATETIME_FORMAT)))#@UndefinedVariable
-    count = 0
-    for period in rf['FcstPeriods']['Period']:
-        #have to check type because json can return list or dict here
-        if isinstance(period['Paragraph'],list):
-            for paragraph in period['Paragraph']:
-                WINDOW.setProperty('Text.Paragraph%d.Title' % count, paragraph['title'].rstrip(':').lstrip('UK Outlook for'))#@UndefinedVariable
-                WINDOW.setProperty('Text.Paragraph%d.Content' % count, paragraph['$'])#@UndefinedVariable
+    try:
+        rf = data['RegionalFcst']
+        issuedat = rf['issuedAt'].rstrip('Z')
+        WINDOW.setProperty('TextForecast.IssuedAt', time.strftime(ISSUEDAT_FORMAT, time.strptime(issuedat, DATAPOINT_DATETIME_FORMAT)))#@UndefinedVariable
+        count = 0
+        for period in rf['FcstPeriods']['Period']:
+            #have to check type because json can return list or dict here
+            if isinstance(period['Paragraph'],list):
+                for paragraph in period['Paragraph']:
+                    WINDOW.setProperty('Text.Paragraph%d.Title' % count, paragraph['title'].rstrip(':').lstrip('UK Outlook for'))#@UndefinedVariable
+                    WINDOW.setProperty('Text.Paragraph%d.Content' % count, paragraph['$'])#@UndefinedVariable
+                    count+=1
+            else:
+                WINDOW.setProperty('Text.Paragraph%d.Title' % count, period['Paragraph']['title'].rstrip(':').lstrip('UK Outlook for'))#@UndefinedVariable
+                WINDOW.setProperty('Text.Paragraph%d.Content' % count, period['Paragraph']['$'])#@UndefinedVariable
                 count+=1
-        else:
-            WINDOW.setProperty('Text.Paragraph%d.Title' % count, period['Paragraph']['title'].rstrip(':').lstrip('UK Outlook for'))#@UndefinedVariable
-            WINDOW.setProperty('Text.Paragraph%d.Content' % count, period['Paragraph']['$'])#@UndefinedVariable
-            count+=1
+    except KeyError as e:
+        e.args = ("Key Error in JSON File", "Key '{0}' not found while processing file from url:".format(e.args[0]), TEXT_FORECAST_URL)
+        raise
     WINDOW.setProperty('TextForecast.IsFetched', 'true')#@UndefinedVariable
 
 @utilities.panelbusy('RightPane')
@@ -125,16 +142,20 @@ def forecastlayer():
         data = json.load(open(filename))
         selection = WINDOW.getProperty('ForecastMap.LayerSelection') or DEFAULT_INITIAL_LAYER#@UndefinedVariable
         #pull parameters out of capabilities file - consider using jsonpath here
-        for thislayer in data['Layers']['Layer']:
-            if thislayer['@displayName'] == selection:
-                layer_name = thislayer['Service']['LayerName']
-                image_format = thislayer['Service']['ImageFormat']
-                default_time = thislayer['Service']['Timesteps']['@defaultTime']
-                timesteps = thislayer['Service']['Timesteps']['Timestep']
-                break
-        else:
-            utilities.log("Couldn't find layer '%s'" % selection)
-            return
+        try:
+            for thislayer in data['Layers']['Layer']:
+                if thislayer['@displayName'] == selection:
+                    layer_name = thislayer['Service']['LayerName']
+                    image_format = thislayer['Service']['ImageFormat']
+                    default_time = thislayer['Service']['Timesteps']['@defaultTime']
+                    timesteps = thislayer['Service']['Timesteps']['Timestep']
+                    break
+            else:
+                utilities.log("Couldn't find layer '%s'" % selection)
+                return
+        except KeyError as e:
+            e.args = ("Key Error in JSON File", "Key '{0}' not found while processing file from url:".format(e.args[0]), FORECAST_LAYER_CAPABILITIES_URL)
+            raise
 
         issuedat = utilities.strptime(default_time, DATAPOINT_DATETIME_FORMAT)
         timestepindex = WINDOW.getProperty('ForecastMap.SliderPosition') or '0'#@UndefinedVariable
@@ -155,7 +176,12 @@ def forecastlayer():
         maptime = issuedat + delta
 
         #get overlay using parameters from gui settings
-        LayerURL = data['Layers']['BaseUrl']['$']
+        try:
+            LayerURL = data['Layers']['BaseUrl']['$']
+        except KeyError as e:
+            e.args = ("Key Error in JSON File", "Key '{0}' not found while processing file from url:".format(e.args[0]), FORECAST_LAYER_CAPABILITIES_URL)
+            raise
+
         url = LayerURL.format(LayerName=layer_name,
                                  ImageFormat=image_format,
                                  DefaultTime=default_time,
