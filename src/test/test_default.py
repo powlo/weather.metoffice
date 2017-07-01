e404e524d4d3824398f3d5fd8d7b78d7c38cb7a5
powlo <paulcbackhouse@gmail.com>
2017-07-01 18:26:34 +0100

    Update tests.

--- src/test/test_default.py
+++ src/test/test_default.py
@@ -8,7 +8,7 @@ class TestDefault(XBMCTestCase):
         from metoffice import utilities
         utilities.failgracefully = lambda f: f
         from metoffice import default
-        
+
         default.properties = Mock()
         default.urlcache.URLCache = Mock()
         default.ADDON = Mock()
@@ -35,51 +35,10 @@ class TestDefault(XBMCTestCase):
         default.main()
         self.assertTrue(default.properties.observation.called)#@UndefinedVariable
         self.assertTrue(default.properties.daily.called)#@UndefinedVariable
-        self.assertFalse(default.properties.threehourly.called)#@UndefinedVariable
-        self.assertFalse(default.properties.forecastlayer.called)#@UndefinedVariable
-        self.assertFalse(default.properties.observationlayer.called)#@UndefinedVariable
-        self.assertFalse(default.properties.text.called)#@UndefinedVariable
-        default.properties.reset_mock()#@UndefinedVariable
-
-        default.CURRENT_VIEW = '3hourly'
-        sys.argv = ['default.py']
-        default.main()
-        self.assertFalse(default.properties.observation.called)#@UndefinedVariable
-        self.assertFalse(default.properties.daily.called)#@UndefinedVariable
         self.assertTrue(default.properties.threehourly.called)#@UndefinedVariable
         self.assertFalse(default.properties.forecastlayer.called)#@UndefinedVariable
         self.assertFalse(default.properties.observationlayer.called)#@UndefinedVariable
         self.assertFalse(default.properties.text.called)#@UndefinedVariable
-        default.properties.reset_mock()#@UndefinedVariable
-
-        default.CURRENT_VIEW = 'forecastmap'
-        default.main()
-        self.assertFalse(default.properties.observation.called)#@UndefinedVariable
-        self.assertFalse(default.properties.daily.called)#@UndefinedVariable
-        self.assertFalse(default.properties.threehourly.called)#@UndefinedVariable
-        self.assertTrue(default.properties.forecastlayer.called)#@UndefinedVariable
-        self.assertFalse(default.properties.observationlayer.called)#@UndefinedVariable
-        self.assertFalse(default.properties.text.called)#@UndefinedVariable
-        default.properties.reset_mock()#@UndefinedVariable
-
-        default.CURRENT_VIEW = 'observationmap'
-        default.main()
-        self.assertFalse(default.properties.observation.called)#@UndefinedVariable
-        self.assertFalse(default.properties.daily.called)#@UndefinedVariable
-        self.assertFalse(default.properties.threehourly.called)#@UndefinedVariable
-        self.assertFalse(default.properties.forecastlayer.called)#@UndefinedVariable
-        self.assertTrue(default.properties.observationlayer.called)#@UndefinedVariable
-        self.assertFalse(default.properties.text.called)#@UndefinedVariable
-        default.properties.reset_mock()#@UndefinedVariable
-
-        default.CURRENT_VIEW = 'text'
-        default.main()
-        self.assertFalse(default.properties.observation.called)#@UndefinedVariable
-        self.assertFalse(default.properties.daily.called)#@UndefinedVariable
-        self.assertFalse(default.properties.threehourly.called)#@UndefinedVariable
-        self.assertFalse(default.properties.forecastlayer.called)#@UndefinedVariable
-        self.assertFalse(default.properties.observationlayer.called)#@UndefinedVariable
-        self.assertTrue(default.properties.text.called)#@UndefinedVariable
         default.properties.reset_mock()#@UndefinedVariable
 
         #Test erase cache setting
@@ -87,4 +46,3 @@ class TestDefault(XBMCTestCase):
         default.main()
         self.assertTrue(default.urlcache.URLCache.return_value.erase.called)#@UndefinedVariable
         default.ADDON.setSetting.assert_called_once_with('EraseCache', 'false')#@UndefinedVariable
-        
--- src/test/xbmctestcase.py
+++ src/test/xbmctestcase.py
@@ -1,7 +1,7 @@
 import unittest
 from mock import patch, Mock
 
-class XBMCTestCase(unittest.TestCase):    
+class XBMCTestCase(unittest.TestCase):
     def setUp(self):
         #Mock up any calls to modules that cannot be imported
         self.xbmc = Mock()
@@ -23,10 +23,18 @@ class XBMCTestCase(unittest.TestCase):
         self.info_labels = {'System.TemperatureUnits' : 'C',
                             'System.BuildVersion' : '16.0 Git:12345678-90a1234'}
 
+        self.region = {'tempunit' : 'C',
+                        'speedunit' : 'mph'}
+
         self.xbmc.getInfoLabel.side_effect = self.mock_getInfoLabel
+
+        self.xbmc.getRegion.side_effect = self.mock_getRegion
 
     def mock_getInfoLabel(self, key):
         return self.info_labels[key]
+
+    def mock_getRegion(self, key):
+        return self.region[key]
 
     def tearDown(self):
         self.module_patcher.stop()
